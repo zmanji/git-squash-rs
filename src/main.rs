e9b1c0d919dce166353f6e48f3a132bb63e03401
Zameer Manji <zmanji@gmail.com>
2020-02-21 22:45:03 -0500

    update dependencies

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ impl error::Error for GitSquashError {
         }
     }
 
-    fn cause(&self) -> Option<&error::Error> {
+    fn cause(&self) -> Option<&dyn error::Error> {
         match *self {
             GitSquashError::Git2(ref e) => Some(e),
             GitSquashError::DirtyRepo => None,
